81b3b0fb56c86018776fa3f89fb2478e19e867ba
oscar <oscar@flashphoner.com>
2023-07-11 16:25:24 +0700

    Fix dependency requirement in manifest #WCS-3862

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FPWCSApi2SwiftWrapper"]),
     ],
     dependencies: [
-        .package(name: "SocketRocket", url: "https://github.com/flashphoner/SocketRocketSPM.git")
+        .package(name: "SocketRocket", url: "https://github.com/flashphoner/SocketRocketSPM.git", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
