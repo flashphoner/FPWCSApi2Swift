bcda92b6089941916f262b7174115b08bc7f779d
admin <admin@Mac-mini-admin.local>
2023-12-28 08:02:11 +0600

    Version 2.6.120

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FPWCSApi2Swift",
-            url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWCSApi2Swift-2.6.118.zip",
-            checksum: "6ee6447aff3b18c132e1cb488a27ce7583997e046af1eff9e0a36d2b5b9e5d2c"
+            url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWCSApi2Swift-2.6.120.zip",
+            checksum: "9f1f643104412daab242827eab9084cf332a5cbeffa136e1dba65bd56bbead54"
         ),
     ]
 )
